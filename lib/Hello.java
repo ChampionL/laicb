7c7fda2aa2f95c73f9d95064b1473f058e345462
ChampionL <lai6803695@163.com>
2013-08-24 15:13:47 +0800

    Update Hello.java

--- lib/Hello.java
+++ lib/Hello.java
@@ -3,5 +3,6 @@ class Hello
   public static void main(String[] args)
   {
       System.out.println("this is a test");
+      System.out.println("welcome to github,this is a file after modiffied by author");
   } 
 }
